ea784520c329af05ef40b6fc12a20a385bc809c8
hyspxt <secondopatto@gmail.com>
2022-08-06 16:42:02 +0200

    game update

---
+++ src/game/game.cpp
@@ -0,0 +1,30 @@
+#include "game.hpp"
+
+#include <ncurses.h>
+#include <ctime>
+#include <cmath>
+#include <unistd.h>
+
+#include "../entity/player.hpp"
+
+Game::Game(){}
+Game::Game(int leftDistance, int rightDistance, int topDistance, int bottomDistance, int startX, int startY){
+    this->leftDistance = leftDistance;
+    this->rightDistance = rightDistance;
+    this->topDistance = topDistance;
+    this->bottomDistance = bottomDistance;
+    this->startX = startX;
+    this->startY = startY;
+
+}
+
+void Game::ncursesSetup(){
+    initscr();
+    cbreak();
+    curs_set(0);
+    noecho();
+    nodelay(stdscr, 1);
+    keypad(stdscr, true);
+    use_default_colors(); // Not essential, but i noticed in some tests some bug so here it is
+    start_color();
+}
--- src/game/game.hpp
+++ src/game/game.hpp
@@ -1,7 +1,25 @@
 #include "../entity/enemy.hpp"
 
-class Game{
+class Game
+{
 
+protected:
+    // Represents the distance between the player and the limit (border of room)
+    // in the specified direction. Serves for collision checks, etc...
+    int leftDistance;
+    int rightDistance;
+    int topDistance;
+    int bottomDistance;
+
+    // These are the starting point for the cursor to draw rooms
+    int startX;
+    int startY;
+
+public:
+    Game();
+    Game(int leftDistance, int rightDistance, int topDistance, int bottomDistance, int startX, int startY);
+
+    void ncursesSetup();
     
 
 };
